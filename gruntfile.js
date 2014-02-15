9a5a260bbdb0e54b24d5c310159a7690be28dede
Laban Eilers <laban@eilers.com>
2014-02-15 10:31:44 -0500

    Removing unused grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -9,49 +9,6 @@ function renameFn(extOld, extNew) {
 module.exports = function (grunt) {
     var config = {
         pkg: grunt.file.readJSON("package.json"),
-        jshint: {
-            uses_defaults: ["gruntfile.js", "js/**/*.js"],
-            with_overrides: {
-                options: {
-                    jshintrc: "test/.jshintrc"
-                },
-                files: {
-                    src: ["test/**/*.js"]
-                }
-            },
-            options: {
-                jshintrc: ".jshintrc"
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 9001,
-                    keepalive: false
-                }
-            }
-        },
-        mocha: {
-            all: {
-                options: {
-                    urls: ["test/*.unittests.html"]
-                }
-            },
-            dialogSmallScreen: {
-                options: {
-                    urls: ["test/jquery.modalDialog.*.unittests.html"]
-                }
-            },
-            specific: {
-                options: {
-                    urls: ["http://localhost:9001/test/jquery.modalDialog.history.unittests.html"]
-                }
-            },
-            options: {
-                reporter: "Spec",
-                timeout: 20000
-            }
-        },
         docco: {
             javascript: {
                 src: ["js/**/*.js"],
@@ -130,45 +87,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-        uglify: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: "dist",
-                    src: ["**.js"],
-                    dest: "dist",
-                    rename: renameFn(".js", ".min.js")
-                }]
-            }
-        },
-        concat: {
-            options: {
-                separator: "\n"
-            },
-            modalDialog: {
-                src: [
-                    "js/jquery.modalDialog.header.js",
-                    "js/jquery.modalDialog.userAgent.js",
-                    "js/jquery.modalDialog.getSettings.js",
-                    "js/jquery.modalDialog.js",
-                    "js/jquery.modalDialog.deviceFixes.js",
-                    "js/jquery.modalDialog.unobtrusive.js",
-                    "js/jquery.modalDialog.history.js"
-                ],
-                dest: "dist/jquery.modalDialog.js"
-            },
-            modalDialogContent: {
-                src: [
-                    "js/jquery.modalDialogContent.header.js",
-                    "js/jquery.modalDialog.userAgent.js",
-                    "js/jquery.modalDialog.getSettings.js",
-                    "js/jquery.modalDialogContent.js",
-                    "js/jquery.modalDialog.deviceFixes.js",
-                    "js/jquery.modalDialog.unobtrusive.js"
-                ],
-                dest: "dist/jquery.modalDialogContent.js"
-            }
-        },
         clean: {
             options: {
                 force: true
@@ -176,17 +94,6 @@ module.exports = function (grunt) {
             build: ["./dist"],
             deploy: ["./.git/docs-temp"],
             docs: ["./site/_site"]
-        },
-        less: {
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: "./css",
-                    src: ["*.less"],
-                    dest: "./dist/css",
-                    rename: renameFn(".less", ".css")
-                }]
-            }
         },
         jekyll: {
             docs: {
@@ -228,64 +135,6 @@ module.exports = function (grunt) {
                     }]
                 }
             }
-        },
-        "strip_code": {
-            options: {},
-            all: {
-                src: "./dist/**.js"
-            }
-        },
-        watch: {
-            modalDialog: {
-                files: ["./js/**/*.modalDialog*.js"],
-                tasks: ["concat:modalDialog", "concat:modalDialogContent", "copy:distSite"]
-            },
-            copyJs: {
-                files: ["./js/**/*.js", "!**modalDialog**"],
-                tasks: ["copy:distJs", "copy:distSite"]
-            },
-            less: {
-                files: ["./css/**/*.less"],
-                tasks: ["less", "copy:distSite"]
-            },
-            jekyll: {
-                files: ["./site/**/*", "!./site/_site/*"],
-                tasks: ["sitePages"]
-            },
-            options: {
-                spawn: false
-            }
-        },
-        jsbeautifier : {
-            all: {
-                src: ["js/**/*.js", "test/**/*.js", "site/javascript/**/*.js"],
-                options: { js: { jslintHappy: true } }
-            }
-        },
-        lineending : {
-            all: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: "./js/",
-                        src: ["./**/*.js"],
-                        dest: "./js/"
-                    }, {
-                        expand: true,
-                        cwd: "./test/",
-                        src: ["./**/*.js"],
-                        dest: "./test/"
-                    }, {
-                        expand: true,
-                        cwd: "./site/javascript/",
-                        src: ["./**/*.js"],
-                        dest: "./site/javascript/"
-                    }
-                ],
-                options: {
-                    eol: "crlf"
-                }
-            }
         }
     };
 
@@ -293,10 +142,6 @@ module.exports = function (grunt) {
     grunt.initConfig(config);
 
     // NPM tasks
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-less");
-    grunt.loadNpmTasks("grunt-mocha");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-connect");
@@ -307,39 +152,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-jekyll");
     grunt.loadNpmTasks("grunt-mkdir");
-    grunt.loadNpmTasks("grunt-strip-code");
-    grunt.loadNpmTasks("grunt-jsbeautifier");
-    grunt.loadNpmTasks("grunt-lineending");
-
-    // Wrap the mocha task
-    grunt.renameTask("mocha", "orig-mocha");
-
-    grunt.registerTask("mocha", function (target) {
-        var config = grunt.config.get("mocha");
-
-        // Turn mocha.files into urls for conrib-mocha
-        var urls = grunt.util._.map(grunt.file.expand(config.all.options.urls), function (file) {
-            return "http://localhost:9001/" + file;
-        });
-
-        config.all.options.urls = urls;
-
-        // Turn mocha.files into urls for conrib-mocha
-        var smallScreenUrls = grunt.util._.map(grunt.file.expand(config.dialogSmallScreen.options.urls), function (file) {
-            return "http://localhost:9001/" + file + "?smallscreen=true";
-        });
-
-        config.dialogSmallScreen.options.urls = smallScreenUrls;
-
-        grunt.config.set("orig-mocha", config);
-
-        var taskName = "orig-mocha";
-        if (target) {
-            taskName += ":" + target;
-        }
-
-        grunt.task.run(taskName);
-    });
 
     grunt.registerTask("connect-keepalive", function () {
         var config = grunt.config.get("connect");
@@ -355,23 +167,11 @@ module.exports = function (grunt) {
 
     grunt.registerTask("default", ["verify", "build"]);
 
-    grunt.registerTask("test", ["less", "connect", "mocha"]);
-
-    //grunt.registerTask("testSpecific", ["less", "connect", "mocha:specific"]);
-
-    grunt.registerTask("verify", ["jshint", "test"]);
-
     grunt.registerTask("copyDist", ["copy:distJs", "copy:distCss", "copy:distOther"]);
-
-    grunt.registerTask("build", ["clean", "less", "copyDist", "concat:modalDialog", "concat:modalDialogContent", "strip_code", "uglify"]);
 
     grunt.registerTask("docs", ["mkdir:docco", "docco", "docco-add-links", "copy:doccoFix"]);
 
     grunt.registerTask("site", ["default", "compress", "sitePages", "docs", "copy:deploy"]);
 
-    grunt.registerTask("siteNoVerify", ["build", "compress", "sitePages", "docs", "copy:deploy"]);
-
     grunt.registerTask("sitePages", ["jekyll", "string-replace:site", "copy:distSite"]);
-
-    grunt.registerTask("beautify", ["jsbeautifier", "lineending"]);
 };
