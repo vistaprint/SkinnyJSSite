cc41de33b02328beeed66f11785f257ea43d32bc
Laban Eilers <laban@eilers.com>
2014-02-15 17:42:36 -0500

    Adding publish script for *nix and README

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,10 +1,26 @@
-grunt siteNoVerify
+cd ../skinny
+
+#generate the dist
+grunt build
 
 if [ $? != 0 ]
 then
     exit $?
 fi
 
+cd ../skinnysite
+
+# Build the site
+grunt
+
+if [ $? != 0 ]
+then
+    exit $?
+fi
+
+# Go back to skinny, where the gh-pages branch is.
+# We need to put the site there
+cd ../skinny
 git checkout gh-pages
 
 if [ $? != 0 ]
@@ -12,10 +28,11 @@ then
     exit $?
 fi
 
+# Clear the directory of anything that could cause a conflict
 git fetch origin
 git reset --hard origin/gh-pages
 
-cp -R .git/docs-temp/* .
+cp -R ../skinnysite/site/_site/* .
 
 git add -A
 git commit -m "Updating documentation"
