b3a0736efdcd815a4d07ef8fcbb81de021ea3be2
Laban Eilers <laban@eilers.com>
2014-02-18 09:58:33 -0500

    Updating publish shell script

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,30 +1,57 @@
-cd ../skinny
+sitepath=`pwd`
+contentpath="../skinny"
+
+if [ "$1" != "" ]; then
+    contentpath=$1
+fi
+
+echo "Writing site from $contentpath to $sitepath"
+
+cd $contentpath
+
+if [ $? != 0 ]
+then
+	echo "Content directory $contentpath could not be found: exiting."
+    exit $?
+fi
+
+#switch the content to the master branch
+git checkout master
+
+if [ $? != 0 ]
+then
+	echo "Could not checkout master branch, probably uncommitted changes: exiting."
+    exit $?
+fi
 
 #generate the dist
 grunt build
 
 if [ $? != 0 ]
 then
+	echo "Grunt build failed for skinny.js: exiting."
     exit $?
 fi
 
-cd ../skinnysite
+cd $sitepath
 
 # Build the site
 grunt
 
 if [ $? != 0 ]
 then
+	echo "Grunt build failed for skinny site: exiting."
     exit $?
 fi
 
 # Go back to skinny, where the gh-pages branch is.
 # We need to put the site there
-cd ../skinny
+cd $contentpath
 git checkout gh-pages
 
 if [ $? != 0 ]
 then
+	echo "Could not checkout gh-pages branch, probably uncommitted changes: exiting."
     exit $?
 fi
 
@@ -32,7 +59,7 @@ fi
 git fetch origin
 git reset --hard origin/gh-pages
 
-cp -R ../skinnysite/site/_site/* .
+cp -R $sitepath/site/_site/* .
 
 git add -A
 git commit -m "Updating documentation"
