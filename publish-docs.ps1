09db969de705530e0cde390f95b71e611b3f1670
Laban Eilers <laban@eilers.com>
2014-02-25 11:02:42 -0500

    Updating powershell publish script

--- publish-docs.ps1
+++ publish-docs.ps1
@@ -1,21 +1,67 @@
-grunt siteNoVerify
+$sitepath=pwd
+$contentpath="../skinny"
 
 if ($LASTEXITCODE -ne  0)
 {
     exit
 }
 
+echo "Writing site from $contentpath to $sitepath"
+
+pushd $contentpath
+
+if ($LASTEXITCODE -ne  0)
+{
+	echo "Content directory $contentpath could not be found: exiting."
+    exit
+}
+
+#switch the content to the master branch
+git checkout master
+
+if ($LASTEXITCODE -ne  0)
+{
+	echo "Could not checkout master branch, probably uncommitted changes: exiting."
+    exit
+}
+
+#generate the dist
+grunt build
+
+if ($LASTEXITCODE -ne  0)
+{
+	echo "Grunt build failed for skinny.js: exiting."
+    exit
+}
+
+pushd $sitepath
+
+# Build the site
+grunt
+
+if ($LASTEXITCODE -ne  0)
+{
+	echo "Grunt build failed for skinny site: exiting."
+    exit
+}
+
+# Go back to skinny, where the gh-pages branch is.
+# We need to put the site there
+pushd $contentpath
 git checkout gh-pages
 
+
 if ($LASTEXITCODE -ne  0)
 {
+	echo "Could not checkout gh-pages branch, probably uncommitted changes: exiting."
     exit
 }
 
+# Clear the directory of anything that could cause a conflict
 git fetch origin
 git reset --hard origin/gh-pages
 
-copy -Force -Recurse .git\docs-temp\* .
+cp $sitepath/site/_site/* . -recurse -force
 
 git add -A
 git commit -m "Updating documentation"
@@ -34,3 +80,6 @@ if ($LASTEXITCODE -ne  0)
 
 git checkout master
 
+popd
+popd
+popd
